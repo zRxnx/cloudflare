77db2a955422b007f5a6de4c1aec6bdd6bf3c9c9
zRxnx <rene.kovacs0712@gmail.com>
2025-06-21 18:46:23 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request) {
     const url = new URL(request.url)
     const host = url.hostname.replace(`.${url.hostname.split('.').slice(-2).join('.')}`, '')
-    const excluded = ["www", "txadmin", "connect"]
+    const excluded = ["www", "txadmin", "connect", "status"]
 
     if (excluded.includes(host)) {
       return fetch(request)  // Keine Weiterleitung für diese Hosts
