8035892f856715a1de900b27f6da5933414b9d25
zRxnx <rene.kovacs0712@gmail.com>
2025-06-21 17:41:14 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+export default {
+  async fetch(request) {
+    const url = new URL(request.url)
+    const host = url.hostname.replace(`.${url.hostname.split('.').slice(-2).join('.')}`, '')
+    const excluded = ["www", "txAdmin", "connect"]
+
+    if (excluded.includes(host)) {
+      return fetch(request)  // Keine Weiterleitung für diese Hosts
+    }
+
+    // Alle anderen Subdomains umleiten
+    url.hostname = "www.zentra.city"
+    return Response.redirect(url.toString(), 301)
+  }
+}
